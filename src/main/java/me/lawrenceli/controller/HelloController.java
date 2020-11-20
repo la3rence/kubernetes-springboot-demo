a30c5e001d3c67c4bf439162af70ab22a60be556
Lawrence Li <lonor@live.com>
2020-11-20 15:39:22 +0800

    add restTemplate

--- src/main/java/me/lawrenceli/controller/HelloController.java
+++ src/main/java/me/lawrenceli/controller/HelloController.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
@@ -16,13 +18,22 @@ public class HelloController {
     @Value("${spring.redis.host}")
     private String redisHost;
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping("/hello")
     public String hello() {
         logger.info("hello method...");
-        return "Hello, Spring!";
+        // 尝试发出 HTTP 请求，测试 Pod 是否可以访问外网：
+        return restTemplate.getForObject("https://lawrenceli.me/_next/data/ve6d6q66WGh3KnfkY191A/blog/long-live-rss.json", String.class);
     }
 
     @GetMapping("/redis")
