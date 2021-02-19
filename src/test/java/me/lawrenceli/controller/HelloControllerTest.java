751d6c01a494c83edef983f0cd833eeb6fca55fd
Lawrence Li <lonor@live.com>
2021-02-19 21:23:38 +0800

    ci: test redis

--- src/test/java/me/lawrenceli/controller/HelloControllerTest.java
+++ src/test/java/me/lawrenceli/controller/HelloControllerTest.java
@@ -1,13 +1,17 @@
 package me.lawrenceli.controller;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -18,6 +22,9 @@ class HelloControllerTest {
     Logger log = LoggerFactory.getLogger(HelloControllerTest.class);
 
     private MockMvc mockMvc;
+
+    @MockBean
+    private StringRedisTemplate stringRedisTemplate;
 
     @BeforeEach
     void setUp() {
@@ -39,9 +46,22 @@ class HelloControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string("Hello, Spring!"));
     }
 
+
+    /**
+     * if redis server isn't provided, use @Disabled("not test: redis server required")
+     */
     @Test
-    @Disabled("not test: redis server required :p")
-    void redis() {
-        log.debug("test redis server io");
+    @DisplayName("should connect to redis and get 200 response")
+    void redis() throws Exception {
+        log.debug("test redis server");
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/redis");
+        ResultActions resultActions = mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        String now = stringRedisTemplate.opsForValue().get("now");
+        if (null != now) {
+            resultActions.andExpect(MockMvcResultMatchers.content().string(now));
+        } else {
+            Assertions.fail();
+        }
     }
 }
